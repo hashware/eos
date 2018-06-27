afc56ce4c6f98a649c7982d59b00f950c44e99ca
hash <hash>
2018-06-27 21:48:00 +0800

    9204098c7934101609e143b3ec50c9af7391c7030a8212c8d7ae97e4428328e7

--- 5_status.sh
+++ 5_status.sh
@@ -3,7 +3,7 @@
 echo -e "\033[42;37m============================================================\033[0m"
 echo -e "\033[42;37m= status =\033[0m"
 
-git status
+git status; echo
 git submodule foreach --recursive "git status; echo"
 
 echo -e "\033[42;37m============================================================\033[0m"
